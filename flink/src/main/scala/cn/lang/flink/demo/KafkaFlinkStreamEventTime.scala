fca7786e5f3024aff3d3af0885da87a7a00d5ab3
lang jiang <langjiang@langs-MacBook-Pro.local>
2020-05-02 23:44:55 +0800

    #0002 add event time

--- flink/src/main/scala/cn/lang/flink/demo/KafkaFlinkStreamEventTime.scala
+++ flink/src/main/scala/cn/lang/flink/demo/KafkaFlinkStreamEventTime.scala
@@ -1,5 +1,6 @@
 package cn.lang.flink.demo
 
+import java.text.SimpleDateFormat
 import java.util.Properties
 
 import org.apache.flink.streaming.api.TimeCharacteristic
@@ -7,9 +8,13 @@ import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.watermark.Watermark
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
+
+import scala.collection.mutable
 
 /**
  * watermark
@@ -17,6 +22,13 @@ import org.apache.flink.streaming.util.serialization.SimpleStringSchema
  * invoke window calculate while watermark > timeWindowDeadline
  **/
 object KafkaFlinkStreamEventTime {
+
+  // 返回的是13位时间戳，精确度到毫秒
+  def dataToTimestamp(date: String): Long = {
+    val sdf = new SimpleDateFormat("yyyy年MM月dd日HH:mm:ss")
+    sdf.parse(date).getTime
+  }
+
   def main(args: Array[String]): Unit = {
     // environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -36,41 +48,60 @@ object KafkaFlinkStreamEventTime {
     kafkaProps.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     kafkaProps.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     kafkaProps.setProperty("auto.offset.reset", "latest")
-    // source
+
+    // source，这里输入的每一条数据格式：uid,2020年05月02日17:26:16
     val transaction = env.addSource(
       new FlinkKafkaConsumer011[String](IN_KAFKA_TOPIC, new SimpleStringSchema(), kafkaProps))
-      .setParallelism(4)
+      .setParallelism(1)
 
     /** transform */
-    val result: DataStream[(Long, Int)] = transaction
-      .map(_.toLong) // 传入的每一条数据都是10位时间戳
+    /* 转换需求是将同一个uid窗口内上报的所有数据 */
+    val result: DataStream[mutable.HashSet[Long]] = transaction
+      .map(iterm => Event(iterm.split(",")(0), dataToTimestamp(iterm.split(",")(1)))) // 传入的每一条数据都是13位时间戳
       .assignTimestampsAndWatermarks(
-        new BoundedOutOfOrdernessTimestampExtractor[Long](Time.milliseconds(1000)) {
-          override def extractTimestamp(element: Long): Long = element * 1000
-        }
-      )
-      .map(iterm => (iterm, 1))
-      .keyBy(0)
-      .timeWindow(Time.seconds(10), Time.seconds(5)) // 10s滑动串口，5s滑动步长
-      .reduce((history, add) => (history._1, history._2 + add._2))
+        new BoundedOutOfOrdernessTimestampExtractor[Event](Time.milliseconds(2000)) {
+          override def extractTimestamp(element: Event): Long = element.timestamp
+        })
+      .keyBy(_.uid)
+      .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+      .fold(new mutable.HashSet[Long]()) {
+        case (set, iterm) => set += iterm.timestamp
+      }
 
     // sink
-    result.map(iterm => iterm.toString() + " : " + System.currentTimeMillis()).print()
+    result.map(iterm => iterm.mkString(";") + " : " + System.currentTimeMillis()).print()
 
     env.execute("KafkaFlinkStreamEventTime")
   }
 }
 
 // 这是业务中数据按照对应格式封装的样例类，其中包含有event time
-case class LogsCase(uid: Long, timestamp: Long) // 这里上报时间戳默认是10位
+case class Event(uid: String, timestamp: Long) // 这里上报时间戳默认是10位
 
-// 自定义获取水位的类，也可以使用BoundedOutOfOrdernessTimestampExtractor匿名实现类
-class MyBoundedOutOfOrdernessTimestampExtractor extends AssignerWithPeriodicWatermarks[LogsCase] {
-  // 获取当前的水位
-  override def getCurrentWatermark: Watermark = ???
+// 低水印机制的类，也可以使用BoundedOutOfOrdernessTimestampExtractor匿名实现类
+class MyBoundedOutOfOrdernessTimestampExtractor(delayInterval: Long) extends AssignerWithPeriodicWatermarks[Event] {
+  // 上一个发送的水印值(也就是上一个触发窗口时的水印值)
+  var lastEmittedWatermark: Long = 0L
+  // 当前进入所有数据中最大的event time和上一次发送水印值的差值
+  var maxOutOfOrderness: Long = delayInterval
+  // 当前进入所有数据中最大的event time
+  var currentMaxTimestamp: Long = lastEmittedWatermark + this.maxOutOfOrderness
+
+  // 获取当前的水印
+  override def getCurrentWatermark: Watermark = {
+    val tmp = this.currentMaxTimestamp - this.maxOutOfOrderness
+    if (tmp >= lastEmittedWatermark) {
+      lastEmittedWatermark = tmp
+    }
+    new Watermark(lastEmittedWatermark)
+  }
 
   // 从数据样例类中抽取时间戳
-  override def extractTimestamp(element: LogsCase, previousElementTimestamp: Long): Long = {
-    null
+  override def extractTimestamp(element: Event, previousElementTimestamp: Long): Long = {
+    val tmp = element.timestamp
+    if (tmp > currentMaxTimestamp) {
+      currentMaxTimestamp = tmp
+    }
+    tmp
   }
 }
