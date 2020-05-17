9fba17ed61bdc872bfec3c7ff635f90992fc2ae4
lang jiang <langjiang@langs-MacBook-Pro.local>
2020-05-17 16:58:39 +0800

    增加交叉链表和删除倒数n节点的方法

--- leetcode/src/main/scala/cn/lang/leetcode/ListNodeCollection.java
+++ leetcode/src/main/scala/cn/lang/leetcode/ListNodeCollection.java
@@ -108,6 +108,62 @@ public class ListNodeCollection {
         return slow.next;
     }
 
+    /**
+     * 160
+     * 地址： https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+     * 找到两个单链表相交的起点
+     * tip： o(1)内存
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // 核心理念是双指针分别遍历A+B和B+A:
+        // 1.首先能确定两次遍历会同时结束，因为长度一样
+        // 2.我们如此划分：A=XA+AB，B=XB+AB，如果A和B相交，那么AB不为空,AB的投即为所求
+        // 3.那么两次遍历路程分别为：
+        //      XA+AB+XB+AB
+        //      XB+AB+XA+AB
+        // 4.所以两次遍历，首次出现相同的相同节点即为所求，可能在第一个AB，也可能在第二个AB(更多可能)
+        if (headA == null || headB == null) return null;
+        ListNode pointerA = headA, pointerB = headB;
+
+        // 不用刻意去考虑循环结束的条件，因为最终都会走完，最终的结果都是null(如果没有相交)
+        while (pointerA != pointerB) {
+            pointerA = pointerA == null ? headB : pointerA.next;
+            pointerB = pointerB == null ? headA : pointerB.next;
+        }
+
+        return pointerA;
+    }
+
+    /**
+     * 19
+     * 地址： https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+     * 删除链表中倒数第n个节点，然后返回头结点
+     * tip： 保证n有效，能使用一趟扫描实现吗？
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 联想到单链表只能从前往后遍历，所以关键问题是如何找出倒数第n个节点的前驱结点
+        // 核心理念是采用双指针：
+        // 1.A和B相差n步长从前往后遍历,当B节点到达链表最后时，A节点即为倒数第n个节点的前驱节点
+        // n1(A) n2 n3(B) n4 n5 n6 n7
+        // n1 n2 n3 n4 n5(A) n6 n7(B)
+        // 2.考虑到如果倒数第n个节点刚好是头结点，那么它的前驱结点是不存在的，可以赋一个哑结点n0
+        // n0(A) n1 n2(B) n3 n4 n5 n6 n7
+        // n0 n1 n2 n3 n4 n5(A) n6 n7(B)
+        ListNode n0 = new ListNode(0);// 哑结点,赋值不影响结果
+        n0.next = head;
+        ListNode A = n0;
+        ListNode B = n0;
+        for (int i = 1; i <= n + 1; i++) {
+            B = B.next;
+        }
+        while (B != null) {
+            A = A.next;
+            B = B.next;
+        }
+        A.next = A.next.next;
+        return n0.next;
+    }
+
     public static void main(String[] args) {
         ListNodeCollection bean = new ListNodeCollection();
 
